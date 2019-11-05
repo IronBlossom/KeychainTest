217a822ffa778a15f5fc59c822aef6b1939f2eeb
Ishtiaq Mahmood Amin <imamin@ael-bd.com>
2019-11-05 22:59:08 +0600

    PODSSSS

--- KeychainTest/ViewController.swift
+++ KeychainTest/ViewController.swift
@@ -7,14 +7,29 @@
 //
 
 import UIKit
+import SwiftKeychainWrapper
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func savePasswordButtonTapped(_ sender: Any) {
+        if let password = passwordTextField.text {
+            let saveSuccessful: Bool = KeychainWrapper.standard.set(password, forKey: "userPassword")
+            print("Save was successful: \(saveSuccessful)")
+            self.view.endEditing(true)
+        }else {
+            print("Input some data first")
+        }
+    }
+    
+    @IBAction func retrievePasswordButtonTapped(_ sender: Any) {
+        let retrievedPassword: String? = KeychainWrapper.standard.string(forKey: "userpassword")
+        print("Retrieved passwork is: \(retrievedPassword!)")
+    }
 }
 
---
+++ Pods/Target Support Files/Pods-KeychainTest/Pods-KeychainTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KeychainTest : NSObject
+@end
+@implementation PodsDummy_Pods_KeychainTest
+@end
